8916ebb5b890a1d7e4cb833f2fbeb5a7131270db
Alexandre Sevilha <alexandre.sevilha1@gmail.com>
2023-07-29 10:00:36 -0300

    Adicionar arquivos de projeto.

---
+++ CriarPDFApp/Entities/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace CriarPDFApp.Entities
+{
+    class Usuario
+    {
+        public string Nome { get; private set; }
+        public string Caminho { get; private set; }
+
+        public Usuario(string nome, string caminho) 
+        {
+            Nome = nome;
+            Caminho = caminho;
+        }
+        
+        public void CriarPDF(string nome, string caminho)
+        {
+            string nomeArquivo = caminho + "\\" + nome + ".PDF"; //Stack para armazenar o endereço para criar o arquivo
+            FileStream arquivoPDF = new FileStream(nomeArquivo, FileMode.Create); //Instancia para criar documentos
+            Document doc = new Document(PageSize.A4); // Instancia para criação do arquivo
+            PdfWriter escritorPDF = PdfWriter.GetInstance(doc, arquivoPDF); //Criação do arquivo
+
+            string dados = "";
+
+            Paragraph paragrafo = new Paragraph(dados, new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL, 14)); //Instancia para criação do texto
+            paragrafo.Add("Parabens " + nome + ", você criou um arquivo PDF"); //Criação do texto
+
+            doc.Open();
+            doc.Add(paragrafo);
+            doc.Close();
+        }
+    }
+}
---
+++ CriarPDFApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using CriarPDFApp.Entities;
+
+Console.WriteLine("Quantos arquivos deseja criar?");
+int qtd = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Por favor digite o endereço do arquivo");
+string caminho = Console.ReadLine();
+
+for (int i = 1; i <= qtd; i++)
+{
+    Console.WriteLine($"Arquivo N°{i}");
+    Console.Write("Por favor digite seu nome: ");
+    string nome = Console.ReadLine();
+
+    Usuario usuario = new Usuario(nome, caminho);
+
+    usuario.CriarPDF(nome, caminho);
+
+    Console.WriteLine("Arquivo PDF criado");
+}
